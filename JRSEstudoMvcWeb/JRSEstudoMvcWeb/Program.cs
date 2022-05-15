3131ed80783192d1c17076d203e797438d4f4681
JhonnatanRodrigues <76016938+JhonnatanRodrigues@users.noreply.github.com>
2022-05-15 03:09:18 -0300

    Foi adicionado a injeçã de dependencia no projeto de estudo

--- JRSEstudoMvcWeb/JRSEstudoMvcWeb/Program.cs
+++ JRSEstudoMvcWeb/JRSEstudoMvcWeb/Program.cs
@@ -1,3 +1,6 @@
+using JRSEstudoMvcWeb_Aplicacao.Calculadora;
+using JRSEstudoMvcWeb_Aplicacao.Calculadora.Iterfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAplicCalculadora, AplicCalculadora>();
 
 var app = builder.Build();
 
